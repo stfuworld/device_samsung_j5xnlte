3ea83a26477d0e5deef480b535276bb01709b00b
Arbab(AB) <arbabaliabforever@gmail.com>
2021-05-21 13:11:59 +0500

    Update lineage_j5xnlte.mk

--- lineage_j5xnlte.mk
+++ lineage_j5xnlte.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/samsung/j5xnlte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5xnlte
-PRODUCT_NAME := lineage_j5xnlte
+PRODUCT_NAME := havoc_j5xnlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J510F
 PRODUCT_MANUFACTURER := samsung
