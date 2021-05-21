30f2916e633b456d1d9e5d1b0c37cf2a854b7841
Arbab(AB) <arbabaliabforever@gmail.com>
2021-05-21 13:11:35 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,10 @@
 LOCAL_PATH := device/samsung/j5xnlte
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/omni_j5xnlte.mk \
-	$(LOCAL_DIR)/rr_j5xnlte.mk \
+	$(LOCAL_DIR)/havoc_j5xnlte.mk \
 	$(LOCAL_DIR)/lineage_j5xnlte.mk
 
 COMMON_LUNCH_CHOICES := \
-	lineage_j5xnlte-eng \
-	lineage_j5xnlte-userdebug \
-	lineage_j5xnlte-user
+	havoc_j5xnlte-eng \
+	havoc_j5xnlte-userdebug \
+	havoc_j5xnlte-user
